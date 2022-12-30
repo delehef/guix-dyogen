effb16f0d0c43b2d95f8469476bd239818176210
Franklin Delehelle <franklin.delehelle@odena.eu>
2022-12-30 17:56:47 +0100

    Add crisprs

--- sylvanite.scm
+++ sylvanite.scm
@@ -21,11 +21,11 @@
    (build-system cargo-build-system)
    (arguments
     `(#:skip-build? #t
-                    #:cargo-inputs
-                    (("rust-pest" ,rust-pest-2)
-                     ("rust-pest-derive" ,rust-pest-derive-2)
-                     ("rust-sorbus" ,rust-sorbus-0.5)
-                     ("rust-thiserror" ,rust-thiserror-1))))
+      #:cargo-inputs
+      (("rust-pest" ,rust-pest-2)
+       ("rust-pest-derive" ,rust-pest-derive-2)
+       ("rust-sorbus" ,rust-sorbus-0.5)
+       ("rust-thiserror" ,rust-thiserror-1))))
    (home-page "https://github.com/delehef/newick")
    (synopsis "Parse, manipulate & write newick-formatted phylogenetic tree")
    (description "A library to read, edit and write newick-formatted trees in rust")
@@ -59,7 +59,7 @@
      (uri
       (git-reference
        (url "https://github.com/delehef/sylvanite")
-       (commit "v1.0.1")))
+       (commit (string-append "v" version))))
      (sha256 (base32 "1ja4r7rbqw5ngdbjrwbzwnbpp32iiavpi6yjn52lysv565pakp65"))))
    (build-system cargo-build-system)
    (arguments
@@ -92,7 +92,7 @@
      (uri
       (git-reference
        (url "https://github.com/delehef/chainsaw")
-       (commit "v1.0.0")))
+       (commit (string-append "v" version))))
      (sha256 (base32 "0asg0arah9zsyi9ghy45g8fpzq056ldxyckjxipff35m52zd3kaf"))))
    (build-system cargo-build-system)
    (arguments
@@ -106,3 +106,34 @@
    (synopsis "Manipulate newick trees")
    (description "A tool suite to manipulate newick-formatted phylogenetic trees")
    (home-page "https://github.com/delehef/chainsaw")
+   (license cecill)))
+
+(define-public crisprs
+  (package
+   (name "crisprs")
+   (version "1.0.3")
+   (source
+    (origin
+     (method git-fetch)
+     (uri
+      (git-reference
+       (url "https://github.com/delehef/crisprs")
+       (commit (string-append "v" version))))
+     (sha256 (base32 "096m33h27dkwrhcy0jqnqy5m5m2xx5d6f3i4a504i46k1j46snlc"))))
+   (build-system cargo-build-system)
+   (arguments
+    `(#:install-source? #f
+      :cargo-inputs
+      ("rust-clap" ,rust-clap-3)
+      ("rust-eyre" ,rust-eyre-0.6)
+      ("rust-lazy-static" ,rust-lazy-static-1)
+      ("rust-log" ,rust-log-0.4)
+      ("rust-maplit" ,rust-maplit-1)
+      ("rust-rayon" ,rust-rayon-1)
+      ("rust-smartstring" ,rust-smartstring-0.2)
+      ("rust-stderrlog" ,rust-stderrlog-0.4)))
+   (native-inputs (list sqlite))
+   (synopsis "Manipulate newick trees")
+   (description "A tool suite to manipulate newick-formatted phylogenetic trees")
+   (home-page "https://github.com/delehef/chainsaw")
+   (license cecill)))
