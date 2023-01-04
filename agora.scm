df9be77341790c8a6a4495e00aa5d2615aa832dd
Franklin Delehelle <delehelle@ens.fr>
2023-01-04 20:58:22 +0000

    update agora

--- agora.scm
+++ agora.scm
@@ -14,8 +14,8 @@
                     (method git-fetch)
                     (uri (git-reference
                           (url "https://github.com/delehef/Agora")
-                          (commit "7d3ecf36b67820a4af7cceacd7c4cf2fb736e7f6")))
-                    (sha256 (base32 "1kpnilkqh2xhbfhg8w6z63r135wji1ifcv9hqqsm0q5nkiz4j451"))))
+                          (commit "791e88c3bcf862426315a7acbf432a7b942fac65")))
+                    (sha256 (base32 "0qyn2r2z93dn7r6ns428r0l1jdpy54q6839c871gx50zy21bp7zb"))))
            (build-system python-build-system)
            (synopsis "AGORA stands for “Algorithm for Gene Order Reconstruction in Ancestors” and was developed by Matthieu Muffato in the DYOGEN Laboratory at the École normale supérieure in Paris in 2008.")
            (description "Algorithm For Gene Order Reconstruction in Ancestors")
